652edb720559bcf4d1365a0452f46f239b9c6d56
SD <sashaluckyman@gmail.com>
2019-11-28 12:26:07 -0800

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       monsters:  [],
       searchField: ''
     };
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount( ) {
@@ -20,7 +20,7 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
-  handleChange (e) {
+  handleChange = (e) => {
     this.setState({ searchField: e.target.value })
   }
 
@@ -31,6 +31,7 @@ class App extends Component {
       );
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monster'
           handleChange={this.handleChange}
