ee2b75e4a363ec8cb7f654c86f6b728d5dc7b0db
SD <sashaluckyman@gmail.com>
2019-11-28 12:22:44 -0800

    Updates

---
+++ precache-manifest.419c174ebe5cbd8c86ec556f984128c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4de20085dcaccbb6405e4ab5cadad057",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "0b1255321ae32f77841e",
+    "url": "/rolodex/static/css/main.e9861ecc.chunk.css"
+  },
+  {
+    "revision": "35e77a4f96ba135e5458",
+    "url": "/rolodex/static/js/2.88369249.chunk.js"
+  },
+  {
+    "revision": "0b1255321ae32f77841e",
+    "url": "/rolodex/static/js/main.cfd98edd.chunk.js"
+  },
+  {
+    "revision": "7a2523d9582604ec00c7",
+    "url": "/rolodex/static/js/runtime-main.5eadda35.js"
+  }
+]);
